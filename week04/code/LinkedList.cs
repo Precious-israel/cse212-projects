c1e2d1f2f5036f9ec84cdbfbc90e588361065f8e
Precious-Israel <carisabiodun@gmail.com>
2026-05-29 22:22:36 +0100

    update the linkedlist file

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -32,7 +32,23 @@ public class LinkedList : IEnumerable<int>
     /// </summary>
     public void InsertTail(int value)
     {
-        // TODO Problem 1
+     // TODO Problem 1
+      // Create new node
+    Node newNode = new(value);
+
+    // If the list is empty, then point both head and tail to the new node.
+    if (_head is null)
+    {
+        _head = newNode;
+        _tail = newNode;
+    }
+    // If the list is not empty, then only tail will be affected.
+    else
+    {
+        newNode.Prev = _tail; // Connect new node to the previous tail
+        _tail!.Next = newNode; // Connect the previous tail to the new node
+        _tail = newNode; // Update the tail to point to the new node
+    }
     }
 
 
@@ -65,6 +81,21 @@ public class LinkedList : IEnumerable<int>
     public void RemoveTail()
     {
         // TODO Problem 2
+
+        // If the list has only one item in it, then set head and tail 
+    // to null resulting in an empty list.
+    if (_head == _tail)
+    {
+        _head = null;
+        _tail = null;
+    }
+    // If the list has more than one item in it, then only the tail
+    // will be affected.
+    else if (_tail is not null)
+    {
+        _tail = _tail.Prev; // Move tail to the previous node
+        _tail!.Next = null; // Disconnect the new tail from the old tail
+    }
     }
 
     /// <summary>
@@ -109,6 +140,34 @@ public class LinkedList : IEnumerable<int>
     public void Remove(int value)
     {
         // TODO Problem 3
+
+        // Search for the node that matches 'value' by starting at the head
+    Node? curr = _head;
+    while (curr is not null)
+      {
+        if (curr.Data == value)
+        {
+            // If the node is the head, use RemoveHead
+            if (curr == _head)
+            {
+                RemoveHead();
+            }
+            // If the node is the tail, use RemoveTail
+            else if (curr == _tail)
+            {
+                RemoveTail();
+            }
+            // If the node is in the middle
+            else
+            {
+                // Connect the previous node to the next node
+                curr.Prev!.Next = curr.Next;
+                curr.Next!.Prev = curr.Prev;
+            }
+            return; // Exit after removing the first occurrence
+        }
+        curr = curr.Next;
+      }
     }
 
     /// <summary>
@@ -117,6 +176,17 @@ public class LinkedList : IEnumerable<int>
     public void Replace(int oldValue, int newValue)
     {
         // TODO Problem 4
+
+         // Search through the entire list
+    Node? curr = _head;
+    while (curr is not null)
+      {
+        if (curr.Data == oldValue)
+        {
+            curr.Data = newValue; // Replace the value
+        }
+        curr = curr.Next; // Continue searching
+      }
     }
 
     /// <summary>
@@ -147,7 +217,14 @@ public class LinkedList : IEnumerable<int>
     public IEnumerable Reverse()
     {
         // TODO Problem 5
-        yield return 0; // replace this line with the correct yield return statement(s)
+
+         var curr = _tail; // Start at the end since this is a backward iteration
+    while (curr is not null)
+      {
+        yield return curr.Data; // Provide each item to the user
+        curr = curr.Prev; // Go backward in the linked list
+      }
+    
     }
 
     public override string ToString()
